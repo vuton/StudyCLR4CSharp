b98f6277509452cf3b5f8ac50463fcbd97c17a22
Vu Ton <vuton101982@gmail.com>
2019-09-02 12:06:21 +0700

    Add project files.

---
+++ ExecuteProgra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExecuteProgra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
